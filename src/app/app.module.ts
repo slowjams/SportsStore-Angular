e49d60c07ec7068dc3162ac97d9c45a81198f8a3
Dong Liu <Luid@INNOPARK.ad>
2019-10-24 12:10:24 +1100

    add event binding, template driven and model-based form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //import { AppComponent } from './app.component';
 import { ProductComponent } from "./component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [ProductComponent]
--- src/app/component.ts
+++ src/app/component.ts
@@ -1,7 +1,8 @@
 import { ApplicationRef, Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
-
+import { ProductFormGroup } from "./form.model";
 
 @Component({
     selector: "app",
@@ -9,18 +10,7 @@ import { Product } from "./product.model";
 })
 export class ProductComponent {
     model: Model = new Model();
-    constructor(ref: ApplicationRef) {
-        (<any>window).appRef = ref;
-        (<any>window).model = this.model;
-    }
-
-    getKey(index: number, product: Product) {
-        return product.id;
-    }
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
+    form: ProductFormGroup = new ProductFormGroup();
 
     getProduct(key: number): Product {
         return this.model.getProduct(key);
@@ -30,28 +20,61 @@ export class ProductComponent {
         return this.model.getProducts();
     }
 
-    getProductCount(): number {
-        console.log("getProductCount invoked");
-        return this.getProducts().length;
+    newProduct: Product = new Product();
+
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
     }
 
-    targetName: string = "Kayak";
-
-    counter: number = 1;
-
-    get nextProduct(): Product {
-        return this.model.getProducts().shift();
+    addProduct(p: Product) {
+        console.log("New Product: " + this.jsonProduct);
     }
 
-    getProductPrice(index: number): number {
-        return Math.floor(this.getProduct(index).price);
+    formSubmitted: boolean = false;
+
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
     }
     
-    // getClassMap(key: number): Object {
-    //     let product = this.model.getProduct(key);
-    //     return {
-    //         "text-center bg-danger": product.name == "Kayak",
-    //         "bg-info": product.price < 50
-    //     };
-    // }
+    /*   unnecessary for template driven form since I'm using model based form
+    getFormValidationMessages(form: NgForm): string[] {
+        console.log('******')
+        console.log(form.controls)
+        let messages: string[] = [];
+        Object.keys(form.controls).forEach(k => {
+            this.getValidationMessages(form.controls[k], k).forEach(m => messages.push(m));
+        });
+        return messages;
+    }
+
+    getValidationMessages(state: any, thingName?: string) {
+        let thing: string = thingName || state.path;
+        let messages: string[] = [];
+        if (state.errors) {
+            for (let errorName in state.errors) {
+                switch (errorName) {
+                    case "required":
+                        messages.push(`You must enter a ${thing}`);
+                        break;
+                    case "minlength":
+                        messages.push(`A ${thing} must be at least
+                                      ${state.errors.minlength.requiredLength}
+                                       characters`);
+                        break;
+                    case "pattern":
+                        messages.push(`The ${thing} contains illegal characters`);
+                        break;
+                }
+            }
+        }
+        return messages;
+    }
+    */
 }
